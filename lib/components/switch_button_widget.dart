7bedddcdcbf2f80ab5f0274b014dc5d33739d6c1
Gabriela Allemão <128430902+gabrielaallemao@users.noreply.github.com>
2025-05-08 09:44:21 -0300

    Update switch_button_widget.dart

--- lib/components/switch_button_widget.dart
+++ lib/components/switch_button_widget.dart
@@ -1,11 +1,21 @@
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
 import 'switch_button_model.dart';
 export 'switch_button_model.dart';
 
 class SwitchButtonWidget extends StatefulWidget {
-  const SwitchButtonWidget({super.key});
+  const SwitchButtonWidget({
+    super.key,
+    required this.isConcluido,
+    this.idLembrete,
+  });
+
+  final bool? isConcluido;
+  final int? idLembrete;
 
   @override
   State<SwitchButtonWidget> createState() => _SwitchButtonWidgetState();
@@ -25,7 +35,7 @@ class _SwitchButtonWidgetState extends State<SwitchButtonWidget> {
     super.initState();
     _model = createModel(context, () => SwitchButtonModel());
 
-    _model.switchLembreteValue = false;
+    _model.switchLembreteValue = widget!.isConcluido!;
     WidgetsBinding.instance.addPostFrameCallback((_) => safeSetState(() {}));
   }
 
@@ -42,18 +52,21 @@ class _SwitchButtonWidgetState extends State<SwitchButtonWidget> {
       width: MediaQuery.sizeOf(context).width * 0.15,
       height: MediaQuery.sizeOf(context).height * 0.15,
       child: Stack(
-        alignment: AlignmentDirectional(0.0, 0.0),
+        alignment: AlignmentDirectional(0, 0),
         children: [
           Align(
-            alignment: AlignmentDirectional(0.0, 0.0),
+            alignment: AlignmentDirectional(0, 0),
             child: Switch.adaptive(
               value: _model.switchLembreteValue!,
-              onChanged: (newValue) async {
-                safeSetState(() => _model.switchLembreteValue = newValue);
-              },
-              activeColor: FlutterFlowTheme.of(context).primary,
-              activeTrackColor: FlutterFlowTheme.of(context).primary,
-              inactiveTrackColor: FlutterFlowTheme.of(context).alternate,
+              onChanged: true
+                  ? null
+                  : (newValue) async {
+                      safeSetState(
+                          () => _model.switchLembreteValue = newValue!);
+                    },
+              activeColor: FlutterFlowTheme.of(context).tertiary,
+              activeTrackColor: FlutterFlowTheme.of(context).tertiary,
+              inactiveTrackColor: FlutterFlowTheme.of(context).accent3,
               inactiveThumbColor:
                   FlutterFlowTheme.of(context).secondaryBackground,
             ),
