b8a675fd135651932371e167cb4dc5c65ce01b8f
Gabriela Allemão <128430902+gabrielaallemao@users.noreply.github.com>
2025-05-08 09:44:39 -0300

    Update switch_button_model.dart

--- lib/components/switch_button_model.dart
+++ lib/components/switch_button_model.dart
@@ -1,8 +1,15 @@
+import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'switch_button_widget.dart' show SwitchButtonWidget;
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class SwitchButtonModel extends FlutterFlowModel<SwitchButtonWidget> {
+  ///  Local state fields for this component.
+
+  bool isConcluido = true;
+
   ///  State fields for stateful widgets in this component.
 
   // State field(s) for SwitchLembrete widget.
