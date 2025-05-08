657f332abcba0acb1a82535b6b342a28977e6da8
Gabriela Allemão <128430902+gabrielaallemao@users.noreply.github.com>
2025-05-08 09:33:14 -0300

    Update criarconta_model.dart

--- lib/pages/criarconta/criarconta_model.dart
+++ lib/pages/criarconta/criarconta_model.dart
@@ -1,7 +1,14 @@
+import '/auth/supabase_auth/auth_util.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
 import '/index.dart';
 import 'criarconta_widget.dart' show CriarcontaWidget;
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class CriarcontaModel extends FlutterFlowModel<CriarcontaWidget> {
   ///  State fields for stateful widgets in this page.
