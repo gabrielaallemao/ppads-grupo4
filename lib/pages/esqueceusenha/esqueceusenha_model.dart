4d3aeb5492e0a5e793c034276969d100bf9094a8
Gabriela Allemão <128430902+gabrielaallemao@users.noreply.github.com>
2025-05-08 09:41:57 -0300

    Create esqueceusenha_model.dart

---
+++ lib/pages/esqueceusenha/esqueceusenha_model.dart
@@ -0,0 +1,29 @@
+import '/auth/supabase_auth/auth_util.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import '/index.dart';
+import 'esqueceusenha_widget.dart' show EsqueceusenhaWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class EsqueceusenhaModel extends FlutterFlowModel<EsqueceusenhaWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode;
+  TextEditingController? emailTextController;
+  String? Function(BuildContext, String?)? emailTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    textFieldFocusNode?.dispose();
+    emailTextController?.dispose();
+  }
+}
